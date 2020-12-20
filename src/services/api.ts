ecfef35d85623c22f3961b1b1bdc8681b7712f23
Tiago <engtiagofranca@gmail.com>
2020-12-20 20:40:03 -0300

    service formosa

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-	// baseURL: 'http://167.249.210.93:9406/api',
-	baseURL: 'http://formosa.agildesenvolvimento.com/api',
+	baseURL: 'http://167.249.210.93:9406/api',
+	// baseURL: 'http://formosa.agildesenvolvimento.com/api',
 });
 
 export default api
